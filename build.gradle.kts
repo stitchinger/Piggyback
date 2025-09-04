7202d3f4707cd2d76739d9faf5cf2a25d8886316
Strokkur24 <strokkur.24@gmail.com>
2025-09-04 22:32:24 +0200

    fix: gradle!

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.papermc.paperweight.userdev.ReobfArtifactConfiguration
 
 plugins {
     id("java")
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
 }
 
 group = "io.georgeous.piggyback"
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
 }
 
 tasks {
